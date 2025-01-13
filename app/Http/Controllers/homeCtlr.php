2f869bccf34810eb542319888f455bf7668e308e
der1der1 <deniel87deniel87@gmail.com>
2025-01-13 13:47:07 +0800

    create_home_controller

--- app/Http/Controllers/homeCtlr.php
+++ app/Http/Controllers/homeCtlr.php
@@ -3,17 +3,51 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\menuModel;
+use App\Models\articleModel;
+use App\Models\albumModel;
+
+
 
 
 class homeCtlr extends Controller
 {
-    public function home_show()
+    public function home_show($page_chose = null)
     {
-        // $user = Auth::user();
         $menus = menuModel::all();
 
-        // 返回結果，傳遞給 welcome.blade.php 視圖
-        return view('deniel_blog', compact('menus'));
-        
+        switch ($page_chose) {
+            case 2:
+                $page_chose = 'content.aboutme';
+                return view('deniel_blog', compact('menus','page_chose'));
+
+            case 3:
+                $travels = articleModel::where('category', 'travel')->inRandomOrder()->get();
+                $page_chose = 'content.travel';
+                return view('deniel_blog', compact('menus','page_chose','travels'));
+
+            case 7:
+                $page_chose = 'content.myweb';
+                return view('deniel_blog', compact('menus','page_chose'));
+
+            case 9:
+                $albums = albumModel::all();
+                $page_chose = 'content.album';
+                return view('deniel_blog', compact('menus','page_chose', 'albums'));
+    
+            default:
+                $homes = articleModel::inRandomOrder()->get();
+                $page_chose = 'content.home';
+                return view('deniel_blog', compact('menus','page_chose','homes'));
+        }
+    }
+
+    public function album_show($album)
+    {
+        $menus = menuModel::all();
+        $album_show = albumModel::where('title', $album)->first();
+
+        $page_chose = 'content.album_show';
+        return view('deniel_blog', compact('menus','page_chose', 'album_show'));
+
     }
 }
